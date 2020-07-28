42740f868135107307b3c33b396f1e4ea32fe98d
Alexandre Pereira <xandymp@gmail.com>
2020-07-28 04:20:11 -0300

    Add dashboard, layout and style

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.logged')
+
+@section('content')
+    <div class="col-sm-12 i-list text-center">
+        <hr/>
+        <h1>Mazzatech-Agenda</h1>
+        <hr/>
+    </div>
+@endsection
