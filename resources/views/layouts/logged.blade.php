ff0dbd5b35d44df7dac56f03a5485a5719844fd8
Alexandre Pereira <xandymp@gmail.com>
2020-07-28 23:22:42 -0300

    Fix dashboard link on sidebar

--- resources/views/layouts/logged.blade.php
+++ resources/views/layouts/logged.blade.php
@@ -80,7 +80,7 @@
         <nav id="sidebar">
             <ul class="list-unstyled components">
                 <li>
-                    <a {{ url('/dashboard') }}>
+                    <a href=" {{ url('/dashboard') }}">
                         <i class="fa fa-home" ></i>
                     </a>
                 </li>
